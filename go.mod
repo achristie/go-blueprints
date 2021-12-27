870b2abc80bf245704a86d08f6ffc64ebc32455b
Andrew Christie <awchristie86@gmail.com>
2021-12-26 19:27:26 -0500

    fixed twitter api.. closing this..

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/achristie/go-blueprints
 go 1.16
 
 require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
 	github.com/gorilla/websocket v1.4.2
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
 	github.com/markbates/goth v1.68.0
+	github.com/nsqio/go-nsq v1.1.0 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 )
--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -1,9 +1,52 @@
-package twittervotes
+package main
 
 import (
+	"encoding/json"
 	"io"
+	"io/ioutil"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/joeshaw/envdecode"
 )
+
+type ts struct {
+	AccessToken string `env:"TWTR_TOKEN,required"`
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+	twtr          ts
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{Transport: &http.Transport{
+			Dial: dial,
+		},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", "Bearer "+twtr.AccessToken)
+
+	return httpClient.Do(req)
+}
+
+func setupTwitterAuth() {
+	if err := envdecode.Decode(&twtr); err != nil {
+		log.Fatalln(err)
+	}
+}
 
 var conn net.Conn
 
@@ -12,7 +55,7 @@ func dial(netw, addr string) (net.Conn, error) {
 		conn.Close()
 		conn = nil
 	}
-	netc, err := net.DialTimeout(netw, addr, 5*time.second)
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
 	if err != nil {
 		return nil, err
 	}
@@ -29,4 +72,81 @@ func closeConn() {
 	if reader != nil {
 		reader.Close()
 	}
+}
+
+type tweet struct {
+	Data struct {
+		ID   string `json:"id"`
+		Text string `json:"text"`
+	} `json:"data"`
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("failed to load options:", err)
+		return
+	}
+
+	u, err := url.Parse("https://api.twitter.com/2/tweets/search/stream")
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		log.Println("creating filter request failed:", err)
+		return
+	}
+	resp, err := makeRequest(req, nil)
+	if err != nil {
+		log.Println("making request failed:", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Fatalln("Failed to connect to Streaming API", string(body), resp.StatusCode)
+	}
+	reader := resp.Body
+	decoder := json.NewDecoder(reader)
+	for {
+		var t tweet
+		if err := decoder.Decode(&t); err != nil {
+			break
+		}
+		log.Println(t)
+		for _, option := range options {
+			if strings.Contains(
+				strings.ToLower(t.Data.Text),
+				strings.ToLower(option),
+			) {
+				log.Println("vote:", option)
+				votes <- option
+			}
+		}
+	}
+}
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("stopping twitter")
+				return
+			default:
+				log.Println("Querying twitter")
+				readFromTwitter(votes)
+				log.Println("waiting...")
+				time.Sleep(10 * time.Second)
+
+			}
+		}
+	}()
+	return stoppedchan
 }
